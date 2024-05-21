4efd14fadfd6eeea37de29e6a223e9b44df1247c
marcojr282 <marco.zapata282@gmail.com>
2024-05-21 13:13:31 -0500

    Exceptiones de los elementos de la clase lugar

--- PCH/src/main/java/co/edu/uco/pch/crosscutting/exceptions/custom/BusinessPCHException.java
+++ PCH/src/main/java/co/edu/uco/pch/crosscutting/exceptions/custom/BusinessPCHException.java
@@ -1,5 +1,18 @@
 package co.edu.uco.pch.crosscutting.exceptions.custom;
 
-public class BusinessPCHException {
+import co.edu.uco.pch.crosscutting.exceptions.PCHException;
+import co.edu.uco.pch.crosscutting.exceptions.enums.Lugar;
+
+public final class BusinessPCHException extends PCHException{
+
+	private static final long serialVersionUID = 2165702387469412553L;
+
+	public BusinessPCHException(final String mensajeUsuario) {
+		super(mensajeUsuario, Lugar.BUSINESS);
+	}
+
+	public BusinessPCHException(final String mensajeTecnico, final String mensajeUsuario, Throwable excepcionRaiz) {
+		super(mensajeTecnico, mensajeUsuario, Lugar.BUSINESS, excepcionRaiz);
+	}
 
 }
